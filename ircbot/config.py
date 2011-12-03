72f9bbe5365e0d1edec3d6b195b1a19936db43ac
Piotr Hajduga <kosarock@gmail.com>
2011-12-03 01:50:38 +0100

    Added configuration file and modules

---
+++ ircbot/config.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+password = None
+nick = "scythebot"
+ident = "scythebot"
+name = "scythebot pythonic IRC bot in development"
+nickserv_passwd = None
+host = "host.com"
+port = 6667
+ssl = False
+channels = [
+		"#test"
+		]
+autorejoin = True
+encoding = "utf-8"
+
+import os.path
+modules_paths = [
+		os.path.expanduser("~/.ircbot/modules/")
+		]
+load_modules = [
+		"admin",
+		"jiggly",
+		"ping",
+		"parrot",
+		"dice"
+		]
+block_modules = []
